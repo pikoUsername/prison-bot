bc15241fc16f58d9f66f818e666ea407e2427220
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-19 19:34:02 +0600

    added i18n to middlewares setup

--- iternal/discord/middlewares/__init__.py
+++ iternal/discord/middlewares/__init__.py
@@ -4,7 +4,8 @@ from loguru import logger
 
 def setup(bot: Bot):
     from .acl import Acl
+    from ..loader import i18n
 
     logger.info("Setuping Middlewares")
     bot.middleware.setup(Acl())
-
+    bot.middleware.setup(i18n)
