dc9b2d9895569e887e909c7db9c96cccd15feadd
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-19 19:33:10 +0600

    added proj_root, and i18n initzilazing

--- iternal/discord/loader.py
+++ iternal/discord/loader.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+
 from data import config
 from pkg.userstorage.memory import MemoryStorage
 
 from ._bot import PrisonRpBot
+from .middlewares.i18n import I18nMiddleware
 
-__all__ = "bot",
+__all__ = "bot", 'i18n', 'proj_root', 'prefix'
 
+# entry point for application is cli/manage.py or cli/__main__.py
+proj_root = Path(__name__).parent.parent.parent
+
+i18n = I18nMiddleware('bot', proj_root / "locals", default='ru')
 prefix = config['bot']['prefix']
 bot = PrisonRpBot(config,
                   storage=MemoryStorage(),
