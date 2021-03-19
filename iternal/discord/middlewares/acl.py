43d79eafed143371a4b3db4f1f516681ccb9770a
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-19 19:21:46 +0600

    added Prison adding to acl, for i18n

--- iternal/discord/middlewares/acl.py
+++ iternal/discord/middlewares/acl.py
@@ -2,6 +2,7 @@ from discord import Message
 from pkg.middlewares.middleware import BaseMiddleware
 
 from iternal.store.user import User
+from iternal.store.prison import Prison
 
 
 class Acl(BaseMiddleware):
@@ -12,7 +13,12 @@ class Acl(BaseMiddleware):
         if not _user:
             await User.create_from_discord(message.author, message.guild.id)
 
+        _prison = await Prison.get(message.guild.id)
+        if not _prison:
+            await Prison.create_from_guild(message.guild)
+
         data['user'] = _user
+        data['prison'] = data['guild'] = None
 
     async def on_pre_process_message(self, message: Message, data: dict):
         await self.setup_chat(message, data)
