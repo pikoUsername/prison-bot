ec2fedbc775401aeb8c54dccceb26bc8f033cf53
CrimsonTod <galymzhan.amantaj@gmail.com>
2021-03-20 12:03:36 +0600

    using instead ~:meth: get, get_prison
    and not using cache for get_user ~:meth:

--- iternal/discord/middlewares/acl.py
+++ iternal/discord/middlewares/acl.py
@@ -9,16 +9,16 @@ class Acl(BaseMiddleware):
     async def setup_chat(self, message: Message, data: dict) -> None:
         user_id = message.author.id
 
-        _user = await User.get_user(user_id)
+        _user = await User.get_user(user_id, False)
         if not _user:
             await User.create_from_discord(message.author, message.guild.id)
 
-        _prison = await Prison.get(message.guild.id)
+        _prison = await Prison.get_prison(message.guild.id)
         if not _prison:
             await Prison.create_from_guild(message.guild)
 
         data['user'] = _user
-        data['prison'] = data['guild'] = None
+        data['prison'] = data['guild'] = _prison
 
     async def on_pre_process_message(self, message: Message, data: dict):
         await self.setup_chat(message, data)
